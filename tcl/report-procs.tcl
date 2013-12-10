a902e06a4cbafa4b3ea357ab4c1a0d3a5ef5073f
Benjamin Brink <tekbasse@yahoo.com>
2013-12-10 12:11:44 -0800

    generalizing formatting procs. wip, untested

--- tcl/report-procs.tcl
+++ tcl/report-procs.tcl
@@ -9,10 +9,17 @@ ad_proc qal_pretty_bytes_iec {
     Returns a pretty number with IEC units in n digits, where first 3 are assumed to be integer.
 } {
     number
-    {decimals "3"}
+    {unit "B"}
+    {significand "3"}
 } {
     # http://en.wikipedia.org/wiki/Orders_of_magnitude_%28data%29
     set abbrev_list [list B KiB MiB GiB TiB PiB EiB ZiB YiB]
+    # convert to units of one
+    set unit_index [lsearch -exact $abbrev_list $unit]
+    if { $unit_index > 0 } {
+        set number [expr { wide( $number ) * pow(1000,$unit_index) } ]
+        set unit "B"
+    }
 #    set units_list \[list bytes kibibyte mebibyte gibibyte tebibyte pebibyte exbibyte zebibyte yobibyte\]
     set base_nbr 1
     set base_exp 0
@@ -23,9 +30,9 @@ ad_proc qal_pretty_bytes_iec {
         }
     }
     set base_bytes [expr { $number / ( $base * 1. ) } ]
-    if { $decimals > 3 } {
-        set extra_decimals [expr { $decimals - 3 } ]
-        set bytes [format "%3.${extra_decimals}f" $base_bytes]
+    if { $significand > 3 } {
+        set extra_significand [expr { $significand - 3 } ]
+        set bytes [format "%3.${extra_significand}f" $base_bytes]
     } else {
         set bytes [format "%3d" [expr { round ( $base_bytes ) } ]]
     }
@@ -34,15 +41,19 @@ ad_proc qal_pretty_bytes_iec {
 }
  
 ad_proc qal_pretty_bytes_dec {
-    Returns a pretty Metric number with byte units in n sigificant digits.
+    Returns a pretty Metric number with byte units in n digits.
 } {
     number
-    significant_digits
-} {
-    number
-    {decimals "3"}
+    {unit "B"}
+    {significand "3"}
 } {
     set abbrev_list [list B kB MB GB TB PB EB ZB YB]
+    # convert to units of one
+    set unit_index [lsearch -exact $abbrev_list $unit]
+    if { $unit_index > 0 } {
+        set number [expr { wide( $number ) * pow(1000,$unit_index) } ]
+        set unit "B"
+    }
 #    set units_list \[list bytes kilobyte megabyte gigabyte terabyte petabyte exabyte zettabyte yottabyte\]
     set base_nbr 1
     set base_exp 0
@@ -53,13 +64,63 @@ ad_proc qal_pretty_bytes_dec {
         }
     }
     set base_bytes [expr { $number / ( $base * 1. ) } ]
-    if { $decimals > 3 } {
-        set extra_decimals [expr { $decimals - 3 } ]
-        set bytes [format "%3.${extra_decimals}f" $base_bytes]
+    if { $significand > 3 } {
+        set extra_significand [expr { $significand - 3 } ]
+        set bytes [format "%3.${extra_significand}f" $base_bytes]
     } else {
         set bytes [format "%3d" [expr { round ( $base_bytes ) } ]]
     }
     set pretty_bytes "$bytes $unit"
     return $pretty_bytes
+}
+
+ad_proc qal_pretty_metric {
+    Returns a pretty, compact Metric number with units in n digits.
+} {
+    number
+    {unit ""}
+    {significand "3"}
+    {ignore_units ""}
+} {
+
+       
+    set abbrev_list [list y z a f p n "&mu;" m c d "" da h k M G T P E Z Y]
+    set ab_pow_list [list -24 -21 -18 -15 -12 -9 -6 -3 -2 -1 0 1 2 3 6 9 12 15 18 21 24]
+    # sometimes &mu; is replaced with mcg..
+    # remove units to ignore
+    if { [string length $ignore_units] > 0 } {
+        set ignore_list [split $ignore_units ", "]
+        foreach i $ignore_list {
+            set ii [lsearch -exact $abbrev_list $i]
+            if { $ii > -1 } {
+                set abbrev_list [lreplace $abbrev_list $i $i]
+                set ab_pow_list [lreplace $ab_pow_list $i $i]
+            }
+        }
+    } 
+    # convert to units of one
+    set unit_index [lsearch -exact $abbrev_list $unit]
+    if { $unit_index > 0 } {
+        set number [expr { wide( $number ) * pow(10,[lindex $ab_pow_list $unit_index]) } ]
+        set unit ""
+    }
+    #    set units_list \[list pico nano micro milli centi deci "" deca hecto kilo mega giga tera \]
+    set base_nbr 1
+    set base_exp 0
+    foreach abbrev $abbrev_list {
+        if { $number > $base } {
+            set base [expr { $base * 1000 } ]
+            set unit $abbrev
+        }
+    }
+    set base_metric [expr { $number / ( $base * 1. ) } ]
+    if { $significand > 3 } {
+        set extra_significand [expr { $significand - 3 } ]
+        set metric [format "%3.${extra_significand}f" $base_metric]
+    } else {
+        set metric [format "%3d" [expr { round ( $base_metric ) } ]]
+    }
+    set pretty_metric "$metric $unit"
+    return $pretty_metric
 }
 
