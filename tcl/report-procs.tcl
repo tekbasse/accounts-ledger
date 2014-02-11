b1eb5838764da32d68e30a3010bbf259a20e2bae
Benjamin Brink <tekbasse@yahoo.com>
2014-02-11 10:56:03 -0800

    revising qal_pretty_metric to latest version of mll_pretty_metric from github.com/tekbasse/mad-lab-lib

--- tcl/report-procs.tcl
+++ tcl/report-procs.tcl
@@ -84,54 +84,63 @@ ad_proc qal_pretty_bytes_dec {
 ad_proc qal_pretty_metric {
     number
     {unit ""}
-    {significand "3"}
+    {significand "1"}
     {ignore_units ""}
 } {
-    Returns a pretty, compact Metric number with units in up to n digits. N must be greater than 3 to trigger floating point.
-} {    
-    if { $number > 1 } {
-        set number [expr { wide( $number ) } ]
-    }
-    set abbrev_list [list y z a f p n "&mu;" m c d "" da h k M G T P E Z Y]
-    set ab_pow_list [list -24 -21 -18 -15 -12 -9 -6 -3 -2 -1 0 1 2 3 6 9 12 15 18 21 24]
-    # sometimes &mu; is replaced with mcg.. 
-    # remove units to ignore 
+    Returns a pretty, compact Metric number with units in up to 3 digit whole numbers. 
+} {
+    set number_abs [expr { abs( $number ) } ]
+
+    # ref: http://en.wikipedia.org/wiki/Metric_prefix#List_of_SI_prefixes
+    # The yy yyy yyyy gglmn and YY YYY YYYY Ggl extensions are nonstandard for SI, but may be useful nevertheless.
+    set abbrev_list [list yyyy gglmn yyy yy y z a f p n "&mu;" m c d "" da h k M G T P E Z Y YY YYY Ggl YYYY]
+    set prefix_list [list quadyocto googolmin triyocto duoyocto yocto zepto atto femto pico nano micro milli centi deci "" deca hecto kilo mega giga tera peta exa zetta yotta duoyotta triyotta googol quadyotta]
+    set ab_pow_list [list -192 -100 -96 -48 -24 -21 -18 -15 -12 -9 -6 -3 -2 -1 0 1 2 3 6 9 12 15 18 21 24 48 96 100 192]
+    # sometimes &mu; is replaced with mcg..
+    # remove units to ignore
     if { [string length $ignore_units] > 0 } {
         set ignore_list [split $ignore_units ", "]
         foreach i $ignore_list {
             set ii [lsearch -exact $abbrev_list $i]
             if { $ii > -1 } {
-                set abbrev_list [lreplace $abbrev_list $i $i]
-                set ab_pow_list [lreplace $ab_pow_list $i $i]
+                set abbrev_list [lreplace $abbrev_list $ii $ii]
+                set ab_pow_list [lreplace $ab_pow_list $ii $ii]
             }
         }
-    }
+    } 
     # convert number to base of one unit (if unit is other than one).
     set unit_index [lsearch -exact $abbrev_list $unit]
     if { $unit_index > -1 } {
         set number [expr { $number * pow(10,[lindex $ab_pow_list $unit_index]) } ]
+        set number_abs [expr { abs( $number ) } ]
         set unit ""
     }
     #    set units_list \[list pico nano micro milli centi deci "" deca hecto kilo mega giga tera \]
-    set test_base_nbr 1e-24
+    set test_base_nbr 1e-192
     set i 0
     foreach abbrev $abbrev_list {
-        if { $number > $test_base_nbr } {
+        if { $number_abs > $test_base_nbr } {
             set base_nbr $test_base_nbr
             set unit $abbrev
             incr i
-            set test_base_nbr [expr { pow(10,[lindex $ab_pow_list $i]) } ]
-        }
+            set test_base_nbr [expr { pow(10,[lindex $ab_pow_list $i]) } ] 
+#            ns_log Notice "testing unit $unit base_nbr $base_nbr test_base_nbr $test_base_nbr i $i"
+        } 
 
     }
-    set base_metric [expr { $number / ( $base_nbr * 1. ) } ]
-    if { $significand > 3 } {
-        set extra_significand [expr { $significand - 3 } ]
-        set metric [format "%3.${extra_significand}f" $base_metric]
+    if { [info exists base_nbr] } {
+        set base_metric [expr { $number / ( $base_nbr * 1. ) } ]
+        if { $significand > 1 } {
+            set extra_significand [expr { $significand - 1 } ]
+            set metric [format "%+3.${extra_significand}f" $base_metric]
+        } else {
+            set metric [format "%+3d" [expr { round( $base_metric ) } ]]
+        }
+        set pretty_metric "${metric} ${unit}"
     } else {
-        set metric [format "%3d" [expr { round ( $base_metric ) } ]]
+        # out of range
+        set pretty_metric [format "%+3.${significand}g" $number]
     }
-    set pretty_metric "$metric $unit"
     return $pretty_metric
 }
 
