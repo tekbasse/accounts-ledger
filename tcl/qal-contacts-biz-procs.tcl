17e544120479392dbb6b5b2e049909ee6650cffe
tekbasse <tekbasse@yahoo.com>
2016-10-06 17:38:43 -0700

    some code..

--- tcl/qal-contacts-biz-procs.tcl
+++ tcl/qal-contacts-biz-procs.tcl
@@ -4,24 +4,49 @@ ad_library {
 
 }
 
-ad_proc qal_contact_create {
+ad_proc qal_contact_write {
     arr_name
 } {
-    Create a contact record
+    Writes a new revision to an existing qal_contact record.
+    If id is empty, creates a new record.
+    A new id is returned if successful.
+    Otherwise empty string is returned.
+    @param array_name
+    @return id or ""
 } {
     upvar 1 instance_id instance_id
-    upvar 1 $arr_name c_arr
-
-
+    upvar 1 $arr_name a_arr
+    qal_contact_defaults arr_name
+    hf_sub_asset_map_defaults arr_name
+    qf_array_to_vars arr_name [qal_contact_keys]
+    qf_array_to_vars arr_name [hf_sub_asset_map_keys]
+    qf_array_to_vars arr_name [list asset_type_id label]
+    if { $type_id eq "" } {
+        set type_id $asset_type_id
+    }
+    set sub_type_id "ns"
+    hf_sub_label_define_empty
+    set attribute_p [qf_is_true $attribute_p 1]
+    set sub_f_id $ns_id
+    set ns_id_new [hf_sub_asset_map_update $f_id $type_id $sub_label $sub_f_id $sub_type_id $attribute_p]
+    if { $ns_id_new ne "" } {
+        # record revision/new
+        set ns_id $ns_id_new
+        db_dml ns_asset_create "insert into qal_contact \
+ ([qal_contact_keys ","]) values ([qal_contact_keys ",:"])"
+    } else {
+        ns_log Warning "qal_contact_write: rejected '[array get arr_name]'"
+    }
+    return $ns_id_new
 }
 
-ad_proc qal_contact_update {
+ad_proc qal_contact_delete {
     arr_name
 } {
-    Update a contact record
+    Delete a contact record
 } {
     upvar 1 instance_id instance_id
-    upvar 1 $arr_name c_arr
+    upvar 1 $arr_name a_arr
 
 
 }
@@ -33,30 +58,35 @@ ad_proc qal_contact_trash {
     Trash a contact record
 } {
     upvar 1 instance_id instance_id
-    upvar 1 $arr_name c_arr
+    upvar 1 $arr_name a_arr
 
 
 }
 
 
-ad_proc qal_customer_create {
+ad_proc qal_customer_write {
     arr_name
 } {
-    Create a customer record
+    Writes a new revision to an existing qal_customer record.
+    If id is empty, creates a new record.
+    A new id is returned if successful.
+    Otherwise empty string is returned.
+    @param array_name
+    @return id or ""
 } {
     upvar 1 instance_id instance_id
-    upvar 1 $arr_name c_arr
+    upvar 1 $arr_name a_arr
 
 
 }
 
-ad_proc qal_customer_update {
+ad_proc qal_customer_delete {
     arr_name
 } {
-    Update a customer record
+    Delete a customer record
 } {
     upvar 1 instance_id instance_id
-    upvar 1 $arr_name c_arr
+    upvar 1 $arr_name a_arr
 
 
 }
@@ -68,30 +98,35 @@ ad_proc qal_customer_trash {
     Trash a customer record
 } {
     upvar 1 instance_id instance_id
-    upvar 1 $arr_name c_arr
+    upvar 1 $arr_name a_arr
 
 
 }
 
 
-ad_proc qal_vendor_create {
+ad_proc qal_vendor_write {
     arr_name
 } {
-    Create a vendor record
+    Writes a new revision to an existing qal_vendor record.
+    If id is empty, creates a new record.
+    A new id is returned if successful.
+    Otherwise empty string is returned.
+    @param array_name
+    @return id or ""
 } {
     upvar 1 instance_id instance_id
-    upvar 1 $arr_name v_arr
+    upvar 1 $arr_name a_arr
 
 
 }
 
-ad_proc qal_vendor_update {
+ad_proc qal_vendor_delete {
     arr_name
 } {
-    Update a vendor record
+    Delete a vendor record
 } {
     upvar 1 instance_id instance_id
-    upvar 1 $arr_name v_arr
+    upvar 1 $arr_name a_arr
 
 
 }
@@ -103,7 +138,7 @@ ad_proc qal_vendor_trash {
     Trash a vendor record
 } {
     upvar 1 instance_id instance_id
-    upvar 1 $arr_name v_arr
+    upvar 1 $arr_name a_arr
 
 
 }
--- tcl/qal-defaults-procs.tcl
+++ tcl/qal-defaults-procs.tcl
@@ -12,7 +12,24 @@ ad_proc qal_contact_defaults {
 } {
     upvar 1 instance_id instance_id
     upvar 1 $arr_name c_arr
-
+    set nowts [dt_systime -gmt 1]
+    set c_list [list instance_id $instance_id \
+                   ns_id "" \
+                   active_p "0" \
+                   name_record "" \
+                   time_trashed "" \
+                   time_created $nowts]
+    set c2_list [list ]
+    foreach {key value} $c_list {
+        lappend c2_list $key
+        if { ![exists_and_not_null c_arr(${key}) ] } {
+            set c_arr(${key}) $value
+        }
+    }
+    if { [llength [set_difference_named_v c2_list [qal_contact_keys]]] > 0 } {
+        ns_log Warning "qal_contact_defaults: Update this proc. \
+It is out of sync with qal_contact_keys"
+    }
     return 1
 }
 
@@ -25,7 +42,24 @@ ad_proc qal_customer_defaults {
 } {
     upvar 1 instance_id instance_id
     upvar 1 $arr_name c_arr
-
+    set nowts [dt_systime -gmt 1]
+    set c_list [list instance_id $instance_id \
+                   ns_id "" \
+                   active_p "0" \
+                   name_record "" \
+                   time_trashed "" \
+                   time_created $nowts]
+    set c2_list [list ]
+    foreach {key value} $c_list {
+        lappend c2_list $key
+        if { ![exists_and_not_null c_arr(${key}) ] } {
+            set c_arr(${key}) $value
+        }
+    }
+    if { [llength [set_difference_named_v c2_list [qal_customer_keys]]] > 0 } {
+        ns_log Warning "qal_customer_defaults: Update this proc. \
+It is out of sync with qal_customer_keys"
+    }
     return 1
 }
 
@@ -38,6 +72,23 @@ ad_proc qal_vendor_defaults {
 } {
     upvar 1 instance_id instance_id
     upvar 1 $arr_name v_arr
-
+    set nowts [dt_systime -gmt 1]
+    set v_list [list instance_id $instance_id \
+                   ns_id "" \
+                   active_p "0" \
+                   name_record "" \
+                   time_trashed "" \
+                   time_created $nowts]
+    set v2_list [list ]
+    foreach {key value} $v_list {
+        lappend v2_list $key
+        if { ![exists_and_not_null v_arr(${key}) ] } {
+            set v_arr(${key}) $value
+        }
+    }
+    if { [llength [set_difference_named_v v2_list [qal_vendor_keys]]] > 0 } {
+        ns_log Warning "qal_vendor_defaults: Update this proc. \
+It is out of sync with qal_vendor_keys"
+    }
     return 1
 }
