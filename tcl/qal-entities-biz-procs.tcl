aaa118cd7782c4693d2fc059356c21982abd2139
Benjamin Brink <tekbasse@yahoo.com>
2017-05-04 21:12:34 -0700

    add more code

--- tcl/qal-entities-biz-procs.tcl
+++ tcl/qal-entities-biz-procs.tcl
@@ -955,59 +955,55 @@ ad_proc -public qal_address_postal_set_primary {
     contact_id
     addrs_id
     {address_type ""}
-    {ignore_postal_constraint_p "0"}
+    {postal_constraint_p "1"}
 } {
-    Set the primary street_addrs_id, mailing_addrs_id, or billing_addrs_id for contact_id.
-    If address_type is unknown, it's determined by reading addrs_id's address_type.
-    If ignore_postal_constraint_p is 1, then any address type can be assigned to
-    a contact's primary street_adds_id is determined by supplied address_type.
+    Set the primary postal address for contact_id. 
+    Address type is one of: street_address mailing_address or billing_address
+    corresponding to qal_contact.street_addrs_id, mailing_addrs_id, or billing_addrs_id.
+
+    If address_type is other than a postal address type, address_type is set to the addrs_id's address_type.
+
+
+    If postal_constraint_p is "0", then any address type can be assigned to
+    the contact's primary postal address.
     Returns 1 if successful, otherwise returns 0.
 } {
     upvar1 instance_id instance_id
     # supplied address_type is target address type
     ##code
     set success_p 0
+    set address_type_list [list street_address mailing_address billing_address]
     set address_type_new ""
-    if { !$ignore_postal_constraint_p } {
-        db_0or1row qal_other_address_map_address_type_r {
-            select record_type as address_type_new from qal_other_address_map
-            where contact_id=:contact_id
-            and addrs_id=:addrs_id
-            and instance_id=:instance_id
-            and trashed_p!='1' }
+    set postal_go_p 1
+    db_0or1row qal_other_address_map_address_type_r {
+        select record_type as address_type_new from qal_other_address_map
+        where contact_id=:contact_id
+        and addrs_id=:addrs_id
+        and instance_id=:instance_id
+        and trashed_p!='1' }
+
+    if { $postal_constraint_p } {
+        if { $address_type_new ni $address_type_list } {
+            ns_log Notice "qal_address_postal_set_primary.987: address_type_new '${address_type_new}' not a postal address. Ignored for instance_id '${instance_id}' contact_id '${contact_id}' addrs_id '${addrs_id}' address_type '${address_type}'."
+            set postal_go_p 0
+        }
     }
-    if { $address_type_new ne "" } {
-        # address exists
 
+    set type_idx [lsearch -exact $address_type_list $address_type]
+    if { $type_idx > -1 && $postal_go_p && $address_type_new ne "" } {
+        # target address validated
 
-
-
-        
         set co_list [qal_contact_read $contact_id]
-
         if { [llength $co_list > 0 ] } {
-            i
-            switch -exact --
-            set name_idx [lsearch -exact ]
+            # contact record exists
+            array set co_arr $co_list
 
-            
-            
-            
-            switch -exact -- $address_type {
-                street_address {
-                    db_dml {
-                        update qal_contact_postal_addrs_id_update1 {
-                            update qal_contact
-                            set street_addrs_id=:addrs_id
-                            where contact_id=:contact_id
-                            and instance_id=:instance_id
-                            and trashed_
-                        }
-                    }
-                }
-                default {
-                    set success_p 0
-                }
+            set ati [lindex [list street_addrs_id mailing_addrs_id billing_addrs_id] $type_idx]
+            set co_arr(${ati}) $addrs_id
+
+            set updated_q [qal_contact_write co2_arr ]
+            if { $updated_q ne "" } {
+                set success_p 1
             }
         }
     }
